e899783e4b5a3ad8d2c0d5e8d71ad559d4ca5966
Henry Pan <hpan@iconectiv.com>
2015-08-14 13:12:38 -0400

    Added test for model component

--- polls/tests.py
+++ polls/tests.py
@@ -10,3 +10,14 @@ class QuestionMethodsTest(TestCase):
         time = timezone.now() + timedelta(days=31)
         future_question = Question(pub_date=time)
         self.assertFalse(future_question.was_published_recently())
+
+    def test_was_published_recently_with_old_questoin(self):
+        time = timezone.now() - timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertFalse(old_question.was_published_recently())
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertTrue(recent_question.was_published_recently())
+
